304369b5324c085b18c16fbad51d590cbf927cc2
Daniel Imaino <cryptodimaino@gmail.com>
2021-05-04 15:13:50 -0700

    Added NLog and fixed the exit method.

--- CalendarRepeater.cs
+++ CalendarRepeater.cs
@@ -20,6 +20,8 @@ namespace CalendarRepeater
 
         private string str;
 
+        private static readonly NLog.Logger _Logger = NLog.LogManager.GetCurrentClassLogger();
+
         public int Years { get; set; }
         public int Months { get; set; }
         public int Days { get; set; }
@@ -30,9 +32,11 @@ namespace CalendarRepeater
         public bool Weekly { get; set; }
         public bool Daily { get; set; }
         public bool Hourly { get; set; }
+        public bool Debug { get; set; }
 
         public CRepeater()
         {
+            ConfigureLogger();
             _RepeatingDate = DateTime.Now;
             _EndDate = null;
             _Monthly = false;
@@ -40,18 +44,30 @@ namespace CalendarRepeater
             _Daily = false;
             _Hourly = false;
             _Minutely = false;
-            _Debug = false;
+            _Debug = true;
             _End = false;
         }
 
         public void CreateCRepeater(Action RunnerMethod)
         {
             _Timer = new Timer(x => { RunnerMethod(); RepeaterFunctionRunner(); }, null, Timeout.Infinite, Timeout.Infinite);
+            
+            if(_Debug)
+            {
+                try
+                {
+                    _Logger.Info("The Calendar Repeater has been created.");
+                }
+                catch(Exception ex)
+                {
+                    _Logger.Error($"Error: {ex}");
+                }
+            }
         }
 
-        public void Debugging()
+        public void DebuggingOff()
         {
-            _Debug = true;
+            _Debug = false;
         }
 
         public void StartDateTimeForRepeater(int year, int month, int day, int hour, int minute, int second)
@@ -63,6 +79,18 @@ namespace CalendarRepeater
                 Console.WriteLine("The Start Date needs to be in the future. Defaulting to DateTime.Now");
                 _RepeatingDate = DateTime.Now;
             }
+
+            if(_Debug)
+            {
+                try
+                {
+                    _Logger.Info($"The Calendar Repeater Start Date for the repeater is: {_RepeatingDate}");
+                }
+                catch(Exception ex)
+                {
+                    _Logger.Error($"Error: {ex}");
+                }
+            }
         }
 
         public void EndDateTimeForRepeater(int year, int month, int day, int hour, int minute, int second)
@@ -73,42 +101,112 @@ namespace CalendarRepeater
             {
                 Console.WriteLine("The End Date needs to be in the future. Defaulting to no End Date.");
             }
+
+            if(_Debug)
+            {
+                try
+                {
+                    _Logger.Info($"The Calendar Repeater End Date for the repeater is: {_RepeatingDate}");
+                }
+                catch(Exception ex)
+                {
+                    _Logger.Error($"Error: {ex}");
+                }
+            }
         }
 
         public void RepeaterMonthly(int day, int hour, int minute)
         {
             _Monthly = true;
             _RepeatingDate = new DateTime(_RepeatingDate.Year, _RepeatingDate.Month, day, hour, minute, 0);
+            if(_Debug)
+            {
+                try
+                {
+                    _Logger.Info($"Monthly Repeater has been set.");
+                }
+                catch(Exception ex)
+                {
+                    _Logger.Error($"Error: {ex}");
+                }
+            }
         }
 
         public void RepeaterWeekly(int day, int hour, int minute)
         {
             _Weekly = true;
             _RepeatingDate = new DateTime(_RepeatingDate.Year, _RepeatingDate.Month, day, hour, minute, 0);
+            if(_Debug)
+            {
+                try
+                {
+                    _Logger.Info($"Weekly Repeater has been set.");
+                }
+                catch(Exception ex)
+                {
+                    _Logger.Error($"Error: {ex}");
+                }
+            }
         }
 
         public void RepeatDaily(int hour, int minute)
         {
             _Daily = true;
             _RepeatingDate = new DateTime(_RepeatingDate.Year, _RepeatingDate.Month, _RepeatingDate.Day, hour, minute, 0);
+        
+            if(_Debug)
+            {
+                try
+                {
+                    _Logger.Info($"Daily Repeater has been set.");
+                }
+                catch(Exception ex)
+                {
+                    _Logger.Error($"Error: {ex}");
+                }
+            }
         }
 
         public void RepeatHouly(int minute)
         {
             _Hourly = true;
             _RepeatingDate = new DateTime(_RepeatingDate.Year, _RepeatingDate.Month, _RepeatingDate.Day, _RepeatingDate.Hour, minute, 0);
+
+            if(_Debug)
+            {
+                try
+                {
+                    _Logger.Info($"Hourly Repeater has been set.");
+                }
+                catch(Exception ex)
+                {
+                    _Logger.Error($"Error: {ex}");
+                }
+            }
         }
 
         public void RepeatEveryNthMinutes(int minute)
         {
             _Minutely = true;
             _Minutes = minute;
+
+            if(_Debug)
+            {
+                try
+                {
+                    _Logger.Info($"Minute Repeater has been set.");
+                }
+                catch(Exception ex)
+                {
+                    _Logger.Error($"Error: {ex}");
+                }
+            }
         }
 
         public void RepeaterFunctionRunner()
         {
             Setup_CRepeater();
-            Console.WriteLine("Press enter to exit.");
+            Console.WriteLine("Type exit end the program.");
         }
 
         public void Setup_CRepeater()
@@ -153,15 +251,22 @@ namespace CalendarRepeater
 
             TimeSpan tickTime = _RepeatingDate - currentTime;
 
-            // Debug
-            if(_Debug == true)
+            Console.WriteLine($"Current Time: {currentTime}");
+            Console.WriteLine($"Repeating Date: {_RepeatingDate}");
+            Console.WriteLine($"End Date: {_EndDate}");
+            Console.WriteLine($"Tick Time: {tickTime}");
+
+            if(_Debug)
             {
-                Console.WriteLine($"Current Time: {currentTime}");
-                Console.WriteLine($"Repeating Date: {_RepeatingDate}");
-                Console.WriteLine($"End Date: {_EndDate}");
-                Console.WriteLine($"Tick Time: {tickTime}");
+                try
+                {
+                    _Logger.Info($"CurrentTime:{currentTime}|RepeatingDate:{_RepeatingDate}|EndDate:{_EndDate != null}|TickTime:{tickTime}");
+                }
+                catch(Exception ex)
+                {
+                    _Logger.Error($"Error: {ex}");
+                }
             }
-            
             _Timer.Change(tickTime, TimeSpan.FromMilliseconds(1));
         }
 
@@ -169,9 +274,33 @@ namespace CalendarRepeater
         {
             do
             {
-                Console.WriteLine("Press enter to exit.");
+                Console.WriteLine("Type exit end the program.");
                 str = Console.ReadLine();
             }while(!str.Equals("exit") && !_End);
+
+            if(_Debug)
+            {
+                try
+                {
+                    _Logger.Info($"Exiting Program {DateTime.Now}");
+                }
+                catch(Exception ex)
+                {
+                    _Logger.Error($"Error: {ex}");
+                }
+            }
+
+            NLog.LogManager.Shutdown();
+        }
+
+        private void ConfigureLogger()
+        {
+            DateTime today = DateTime.Now;
+            
+            var config = new NLog.Config.LoggingConfiguration();
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = $"../../../Logs/CalendarRepeater/{today.Year}/{today.ToString("MMM")}/{today.Day.ToString("00")}.txt" };
+            config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, logfile);
+            NLog.LogManager.Configuration = config;
         }
     }
 }
